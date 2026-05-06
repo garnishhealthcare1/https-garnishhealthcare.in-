392d2976ba034b933091a3854fcc702ff8b42196
garnishhealthcare1 <gdchealthcare1@gmail.com>
2026-05-06 15:41:40 +0530

    Delete script.js

--- script.js
+++
@@ -1,7 +0,0 @@
-window.addEventListener("scroll",()=>{
-document.querySelectorAll("section").forEach(sec=>{
-if(sec.getBoundingClientRect().top < window.innerHeight-100){
-sec.classList.add("show");
-}
-});
-});
